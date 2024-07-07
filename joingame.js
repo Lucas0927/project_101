ef915a52ec683314213a917969c73179369fd66b
SungMin Park <spark259@asu.edu>
2024-07-07 12:41:51 -0700

    real-time ability, players or host leaving and deleting the room, advanced join code.

--- joingame.js
+++ joingame.js
@@ -1,8 +1,9 @@
 import { db } from './firebase-config.js';
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+import { collection, addDoc, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
 document.getElementById('roomCode').addEventListener('input', function() {
-    const code = this.value;
+    const code = this.value.toUpperCase();
+    this.value = code; // Set the input value to uppercase
     const joinButton = document.getElementById('joinButton');
     if (code.length === 6) {
         joinButton.disabled = false;
@@ -12,9 +13,20 @@ document.getElementById('roomCode').addEventListener('input', function() {
 });
 
 document.getElementById('joinButton').addEventListener('click', async function() {
-    const roomCode = document.getElementById('roomCode').value;
+    const roomCode = document.getElementById('roomCode').value.toUpperCase();
+
+    // Check if the room exists
+    const roomRef = doc(db, 'rooms', roomCode);
+    const roomDoc = await getDoc(roomRef);
+
+    if (!roomDoc.exists()) {
+        // Show a message that says the room does not exist
+        alert("The room does not exist. Try different code.");
+        return;
+    }
+
     const playerRef = await addDoc(collection(db, 'rooms', roomCode, 'players'), {
-        username: 'none' // Username will be added on the next page
+        username: 'Joining...' // Username will be added on the next page
     });
     const userId = playerRef.id; // Use Firestore auto-ID
 
